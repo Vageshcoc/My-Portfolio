7c565daa1fc2e9a92a2ae751619e1e84de81d0c8
Vagesh <vageshcoc@gmail.com>
2024-02-14 13:40:26 +0530

    final commit

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 /*================================= typed js ==============================*/
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Final Year Student', 'Athlete','Developer'],
+    strings: ['Final Year Student','Full Stack Developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
